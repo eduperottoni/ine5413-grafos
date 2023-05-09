c91feb633b95c25456741f95039753a7b70e3cbd
Eduardo Perottoni <edu.perottoni@gmail.com>
2023-05-09 09:10:31 -0300

    Inserting kruskal algorithm

--- atividade-a2/algoritmos/kruskal.py
+++ atividade-a2/algoritmos/kruskal.py
@@ -6,7 +6,6 @@ def kruskal(arquivo: str):
     Implementação do algoritmo de Kruskal para encontrar árvore mínima do grafo
     A implementação utiliza estrutura de dados que com maior eficiência, para redução
     da complexidade
-    https://edisciplinas.usp.br/pluginfile.php/4627613/mod_resource/content/1/ACH2024-Aula10-ArvoreGeradoraMinima_AlgKruskal.pdf
     """
     grafo = Grafo(arquivo)
     A = []
@@ -14,9 +13,10 @@ def kruskal(arquivo: str):
     for _ in grafo.vertices:
         S.append(CDElemento())
     arestas_ordenadas = sorted(grafo.arestas, key=lambda x: x.peso, reverse=False)
-    for _, aresta in enumerate(arestas_ordenadas):
-        print(aresta)
-        if S[aresta.vertice1.id].CDEncontrar(S[aresta.vertice1.id]) != S[aresta.vertice1.id].CDEncontrar(S[aresta.vertice2.id]):
+    for aresta in arestas_ordenadas:
+        index_v1 = aresta.vertice1.id - 1
+        index_v2 = aresta.vertice2.id - 1
+        if S[index_v1].CDEncontrar(S[index_v1]) != S[index_v1].CDEncontrar(S[index_v2]):
             A.append(aresta)
-            S[aresta.vertice1.id].CDUniao(S[aresta.vertice1.id], S[aresta.vertice2.id])
+            S[index_v1].CDUniao(S[index_v1], S[index_v2])
     return A
--- atividade-a2/main.py
+++ atividade-a2/main.py
@@ -7,4 +7,16 @@ arquivo_agm = 'src/agm_tiny.net'
 
 grafo = Grafo(arquivo_grafo_orientado)
 
-kruskal(arquivo_agm)
+
+def print_questao1(arquivo: str) -> None:
+    print('--- QUESTÃO 1 ---')
+    arestas_agm = kruskal(arquivo)
+    custo = 0
+    arestas_formatadas = []
+    for aresta in arestas_agm:
+        custo += aresta.peso
+        arestas_formatadas.append(f'{aresta.vertice1.id}-{aresta.vertice2.id}')     
+    print(custo)
+    print(str(arestas_formatadas).replace('[','').replace(']','').replace("'",''))
+
+print_questao1(arquivo_agm)
