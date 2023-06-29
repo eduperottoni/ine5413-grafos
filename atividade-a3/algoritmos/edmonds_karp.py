70f4aed7345497ea0069865377a3add0472770e7
Eduardo Perottoni <edu.perottoni@gmail.com>
2023-06-28 22:51:27 -0300

    Implementação de Edmonds-Karp

--- atividade-a3/algoritmos/edmonds_karp.py
+++ atividade-a3/algoritmos/edmonds_karp.py
@@ -2,13 +2,10 @@
 # Crie um programa que receba um grafo dirigido e ponderado como argumento. Ao final, 
 # imprima na tela o valor do fluxo máximo resultante da execução do algoritmo de Edmonds-Karp.
 from collections import deque
-from estruturas.grafo import Grafo
-from estruturas.aresta import Aresta
 
 def edmonds_karp(grafo, v_inicial, v_final):
     fluxo = 0
     F = [[0 for _ in range(grafo.qtdVertices + 1)] for _ in range(grafo.qtdVertices + 1)]
-    fila = None
     while True:
         c_aumentante = [-1 for _ in range(grafo.qtdVertices + 1)]
         c_aumentante[v_inicial] = -1
@@ -19,7 +16,7 @@ def edmonds_karp(grafo, v_inicial, v_final):
         fluxoCaminho, c_aumentante = BFS(
             grafo, F, fila, c_aumentante, cap_residual, v_final
         )
-
+        
         if fluxoCaminho == 0: break
 
         fluxo += fluxoCaminho
@@ -31,108 +28,28 @@ def edmonds_karp(grafo, v_inicial, v_final):
             F[v_atual][vizinho] -= fluxoCaminho
             v_atual = vizinho
 
-    print(f"Fluxo máximo {v_inicial} -> {v_final}: {fluxo}")
-            
-    # C = [False for _ in range(grafo.qtdVertices)]
-    # A = [None for _ in range(grafo.qtdVertices)]
+    return fluxo
 
-    # C[v_inicial.id - 1] = True
-    
-    # Q = deque([v_inicial])
-
-    # while len(Q):
-    #     u = Q.popleft()
-
-    #     for v in v_inicial.vizinhos.keys():
-    #         if C[v.id - 1] == False and grafo_f.peso(u, v) > 0:
-    #             C[v.id - 1] = True
-    #             A[v.id - 1] = u
-
-    #             if v == v_final:
-    #                 p = [v_final]
-    #                 w = v_final
-
-    #                 while w != v_inicial:
-    #                     w = A[w.id - 1]
-    #                     p.append(w)
-    #                 return p
-    #             Q.deque(v)
-    
-#     return None
-
-def BFS(grafo, grafo_residual, fila, caminhoAumentante, capacidadeResidual, final):
+def BFS(grafo, grafo_residual, fila, caminho_aumentante, cap_residual, final):
     while len(fila) > 0:
-        verticeAtual = fila.popleft()
-
-        for vizinho in grafo.vizinhos(int(verticeAtual)).keys():
+        vertice_atual = int(fila.popleft())
+        for vizinho in list(grafo.vizinhos(vertice_atual).keys()):
+            vizinho = int(vizinho)
             residual = (
-                grafo.peso(int(verticeAtual), int(vizinho))
-                - grafo_residual[int(verticeAtual)][int(vizinho)]
+                grafo.peso(vertice_atual, vizinho)
+                - grafo_residual[vertice_atual][vizinho]
             )
             
-            if residual > 0 and caminhoAumentante[int(vizinho)] == -1:
-                caminhoAumentante[int(vizinho)] = verticeAtual
-                capacidadeResidual[int(vizinho)] = min(
-                    capacidadeResidual[int(verticeAtual)],
+            if residual > 0 and caminho_aumentante[vizinho] == -1:
+                caminho_aumentante[vizinho] = vertice_atual
+                cap_residual[vizinho] = min(
+                    cap_residual[vertice_atual],
                     residual
                 )
                 
                 if vizinho is not final:
                     fila.append(vizinho)
                 else:
-                    return capacidadeResidual[final], caminhoAumentante
-    return 0, caminhoAumentante
-
-
-
-
-# from collections import (
-#     deque,
-# )  # https://docs.python.org/3/library/collections.html#collections.deque
-# from Grafo.Grafo import Grafo
-
-
-# class EdmondsKarp:
-#     def __init__(self, grafo: Grafo, inicial: int, final: int):
-#         self.__grafo = grafo
-#         self.__inicial = inicial
-#         self.__final = final
-
-#         self.__fluxo = -0
-#         self.__F = [
-#             [0 for _ in range(self.__grafo.qtdVertices() + 1)]
-#             for _ in range(self.__grafo.qtdVertices() + 1)
-#         ]
-
-#     def run(self):
-#         while True:
-#             A = [-1 for _ in range(self.__grafo.qtdVertices() + 1)]
-#             caminhoAumentante[self.__inicial] = -1
-
-#             C = [0 for _ in range(self.__grafo.qtdVertices() + 1)]
-#             capacidadeResidual[self.__inicial] = float("inf")
-            
-#             self.__fila = deque([self.__inicial])
-
-#             fluxoCaminho, caminhoAumentante = self.BFS(
-#                 caminhoAumentante, capacidadeResidual
-#             )
-            
-#             if fluxoCaminho == 0:
-#                 break
-        
-#             self.__fluxo += fluxoCaminho
-#             verticeAtual = self.__final
-            
-#             while verticeAtual != self.__inicial:
-#                 vizinho = caminhoAumentante[verticeAtual]
-#                 self.__F[vizinho][verticeAtual] += fluxoCaminho
-#                 self.__F[verticeAtual][vizinho] -= fluxoCaminho
-#                 verticeAtual = vizinho
-    
-#         print(f"Fluxo máximo {self.__inicial} -> {self.__final}: {self.__fluxo}")
-
-    
-        
-#         return 0, caminhoAumentante
+                    return cap_residual[final], caminho_aumentante
+    return 0, caminho_aumentante
 
--- atividade-a3/main.py
+++ atividade-a3/main.py
@@ -10,16 +10,12 @@ teste_fluxo1 = 'src/fluxo_maximo/db4096.net'
 teste_fluxo2 = 'src/fluxo_maximo/fluxo_maximo_aula.net'
 teste_fluxo3 = 'src/fluxo_maximo/wiki.net'
 
-# def print_questao1(arquivo_emparelhamento):
-#     print('--- QUESTÃO 1 ---')
-#     grafo = Grafo(arquivo_emparelhamento)
-
-#     print(edmonds_karp(grafo, 4, 5, grafo.rede_residual()))
-
-def print_questao1(arquivo_fluxo_maximo):
+def print_questao1(arquivo_fluxo_maximo, v_inicial, v_final):
     print('--- QUESTÃO 1 ---')
     grafo = Grafo(arquivo_fluxo_maximo)
-    edmonds_karp(grafo, 1, 5)
+    fluxo = edmonds_karp(grafo, v_inicial, v_final)
+    print(f'O fluxo máximo de {v_inicial} para {v_final} é {fluxo}')
+    # print(f"Fluxo máximo {v_inicial} -> {v_final}: {fluxo}")
 
 # def print_questao2(arquarquivo_emparelhamentoivo):
 #     print('--- QUESTÃO 2 ---')
@@ -28,4 +24,6 @@ def print_questao1(arquivo_fluxo_maximo):
 #     print(hopcroft_karp(grafo))
 
 # print_questao2(arquivo_emparelhamento)
-print_questao1(teste_fluxo3)
+print_questao1(teste_fluxo3, 1, 7)
+print_questao1(teste_fluxo1, 1, 7)
+print_questao1(teste_fluxo2, 1, 6)
